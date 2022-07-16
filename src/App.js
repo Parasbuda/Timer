1ba047de9a109786c4bcd94491bcaa4016ac09ec
Parasbuda <parasbuda1234@gmail.com>
2022-07-16 21:53:00 +0545

    updated

--- src/App.js
+++ src/App.js
@@ -5,26 +5,46 @@ import Timer from "./Pages/Timer";
 function App() {
   const [time, setTime] = useState([0, 0]);
   const [timerOn, setTimerOn] = useState([false, false]);
-  let interval = [null, null];
+  const [interval, setInterval] = useState([null, null]);
   useEffect(() => {
     for (let i = 0; i < timerOn.length; i++) {
       if (timerOn[i]) {
-        interval[i] = setInterval(() => {
-          const updated = time.map((tim, index) => {
-            if (i === index) {
-              tim = tim + 10;gi
-            }
-            return tim;
-          });
-          setTime(updated);
-        }, 10);
+        const updatedInterval = interval.map((inter, ind) => {
+          if (i === ind) {
+            inter = setInterval(() => {
+              const updated = time.map((tim, index) => {
+                if (i === index) {
+                  tim = tim + 10;
+                }
+                return tim;
+              });
+              setTime(updated);
+            }, 10);
+          }
+          return inter;
+        });
+        setInterval(updatedInterval);
       } else if (!timerOn[i]) {
         clearInterval(interval[i]);
+        const updated = interval.map((inter, index) => {
+          if (i === index) {
+            inter = null;
+          }
+          return inter;
+        });
+        setInterval(updated);
       }
     }
     return () => {
       for (let i = 0; i < timerOn.length; i++) {
         clearInterval(interval[i]);
+        const updated = interval.map((inter, index) => {
+          if (i === index) {
+            inter = null;
+          }
+          return inter;
+        });
+        setInterval(updated);
       }
     };
   }, [timerOn, time]);
